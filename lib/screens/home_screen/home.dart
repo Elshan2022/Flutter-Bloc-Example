5889eaa89cad6671f78fb807e55d4470057eb86b
Elshan2022 <h.elsen.h57@gmail.com>
2024-04-18 19:25:11 +0400

    Home Page UI

--- lib/screens/home_screen/home.dart
+++ lib/screens/home_screen/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_example/constants/colors.dart';
+import 'package:flutter_bloc_example/constants/text_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +8,68 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home screen"),
+      appBar: PreferredSize(
+        preferredSize: const Size(double.maxFinite, 150),
+        child: AppBar(
+          flexibleSpace: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  leading: InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQlj3rCfLHry58AtJ8ZyBEAFPtChMddDSUSjt7C7nV3Nhsni9RIx5b0-n7LxfgerrPS6b-P-u3BOM3abuY",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    "Cristiano Ronaldo",
+                    style: TextStyles.sameBold(
+                      color: AppColors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                  subtitle: Row(
+                    children: [
+                      const Icon(
+                        Icons.location_on,
+                        color: AppColors.white,
+                        size: 14,
+                      ),
+                      const SizedBox(width: 5),
+                      Text(
+                        "Porto, Portugal",
+                        style: TextStyles.medium(
+                          color: AppColors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.notifications_on_sharp,
+                      color: AppColors.white,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -3,17 +3,36 @@ import 'package:flutter_bloc_example/constants/colors.dart';
 
 class AppTheme {
   static ThemeData appTheme = ThemeData(
-    elevatedButtonTheme: ElevatedButtonThemeData(
+      elevatedButtonTheme: _elevatedButtonTheme(),
+      colorScheme: _colorScheme(),
+      appBarTheme: _appBarTheme());
+
+  static ColorScheme _colorScheme() {
+    return ColorScheme.fromSeed(
+      seedColor: AppColors.primaryDark,
+      background: AppColors.white,
+    );
+  }
+
+  static AppBarTheme _appBarTheme() {
+    return const AppBarTheme(
+        backgroundColor: AppColors.primaryDark,
+        centerTitle: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(25),
+          ),
+        ));
+  }
+
+  static ElevatedButtonThemeData _elevatedButtonTheme() {
+    return ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(10)),
         ),
         backgroundColor: AppColors.primaryDark,
       ),
-    ),
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: AppColors.primaryDark,
-      background: AppColors.white,
-    ),
-  );
+    );
+  }
 }
